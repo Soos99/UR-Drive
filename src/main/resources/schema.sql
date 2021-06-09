58820f94b89d132a0e4183c84453eed1fa29b52f
Soos99 <hoangnbk07@gmail.com>
2021-06-09 20:26:01 +0700

    add db schema for notes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,14 +8,14 @@ CREATE TABLE IF NOT EXISTS STUDENTS
     last_name  VARCHAR(20) NOT NULL
 );
 
--- CREATE TABLE IF NOT EXISTS NOTES
--- (
---     noteid          INT PRIMARY KEY auto_increment,
---     notetitle       VARCHAR(20),
---     notedescription VARCHAR(1000),
---     userid          INT,
---     foreign key (userid) references USERS (userid)
--- );
+CREATE TABLE IF NOT EXISTS NOTES
+(
+    note_id          INT PRIMARY KEY auto_increment,
+    note_title       VARCHAR(20),
+    note_description VARCHAR(1000),
+    student_id          INT,
+    foreign key (student_id) references STUDENTS (student_id)
+);
 --
 -- CREATE TABLE IF NOT EXISTS FILES
 -- (
