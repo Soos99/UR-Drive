a2238e6859a896b26d8b0ffeaa41516d6f8c8eeb
Soos99 <hoangnbk07@gmail.com>
2021-06-09 17:09:40 +0700

    add structure

---
+++ src/main/java/com/springmvc/urdrive/UrdriveApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc.urdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrdriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrdriveApplication.class, args);
+    }
+
+}
