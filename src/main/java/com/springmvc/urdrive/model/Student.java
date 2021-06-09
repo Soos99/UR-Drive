09d5d30d856c464b2828dab46e05696c4d3b0f7a
Soos99 <hoangnbk07@gmail.com>
2021-06-09 19:04:14 +0700

    Add annotation to match fields in db

--- src/main/java/com/springmvc/urdrive/model/Student.java
+++ src/main/java/com/springmvc/urdrive/model/Student.java
@@ -2,20 +2,29 @@ package com.springmvc.urdrive.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
+@Table(name = "STUDENTS")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
     private Integer studentID;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "salt")
     private String salt;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 }
--- src/main/java/com/springmvc/urdrive/service/StudentService.java
+++ src/main/java/com/springmvc/urdrive/service/StudentService.java
@@ -7,10 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.security.SecureRandom;
-import java.util.Base64;
-import java.util.Optional;
-
 @Service
 @Data
 @Slf4j
