51468f28a4f07ea3c6f604bc99155149b646a1b9
Soos99 <hoangnbk07@gmail.com>
2021-06-09 21:04:00 +0700

    add controller for file

--- src/main/java/com/springmvc/urdrive/controller/HomeController.java
+++ src/main/java/com/springmvc/urdrive/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.springmvc.urdrive.controller;
 
+import com.springmvc.urdrive.model.DriveFile;
 import com.springmvc.urdrive.model.Note;
+import com.springmvc.urdrive.service.DriveFileService;
 import com.springmvc.urdrive.service.NoteService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -18,6 +21,9 @@ import java.security.Principal;
 public class HomeController {
     @Autowired
     private NoteService noteService;
+    @Autowired
+    private DriveFileService driveFileService;
+
     @GetMapping({"/","/home"})
     public String getHomePage(Model model, Principal principal) {
         if (model.getAttribute("active") == null) {
@@ -25,8 +31,22 @@ public class HomeController {
         }
 //        model.addAttribute("credentials", credentialService.getCreds(principal.getName()));
         model.addAttribute("notes", noteService.getNotes(principal.getName()));
-//        model.addAttribute("files", driveFileService.getDriveFiles(principal.getName()));
+        model.addAttribute("files", driveFileService.getDriveFiles(principal.getName()));
         return "home";
+    }
+
+    @PostMapping("/add-file")
+    public String addFile(@RequestParam("file") MultipartFile multipartFile, Principal principal, RedirectAttributes redirectAttributes) {
+        driveFileService.addDriveFile(multipartFile, principal.getName());
+        redirectAttributes.addFlashAttribute("active", "file");
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete-file")
+    public String deleteFile(@RequestParam("id") Integer fileID, RedirectAttributes redirectAttributes) {
+        driveFileService.deleteDriveFile(fileID);
+        redirectAttributes.addFlashAttribute("active", "file");
+        return "redirect:/";
     }
 
     @PostMapping("/update-note")
